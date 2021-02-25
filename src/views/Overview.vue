3186f3894aa3c63aafac9da6ac50025614482e63
Sarah Berg <sarahvb@stud.ntnu.no>
2021-02-25 14:13:29 +0100

    test in overview.vue

---
+++ src/views/Overview.vue
@@ -0,0 +1,15 @@
+<template>
+
+<div class="container-fluid">
+    <div class="container">
+        <div class="row align-items-center" style="height:700px;">
+            <div class="col-md-4">
+                <h1>dd</h1>
+            </div>
+            <div class="col-md-3">hei</div>
+            <div class="col-md-5">ho</div>
+        </div>
+    </div>
+</div>
+
+</template>
