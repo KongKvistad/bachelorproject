64caba74a7e8bf6a9024480c6605d34bafbe01a4
Sarah Berg <sarahvb@stud.ntnu.no>
2021-02-25 13:16:11 +0100

    added bootstrap dependency

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import router from './router'
 import store from './store'
 import { auth } from './firebase'
 import './assets/scss/app.scss'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 Vue.component('v-select', vSelect)
