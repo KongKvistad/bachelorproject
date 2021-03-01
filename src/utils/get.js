4374aa5d8c250544f188dc12fbd37ed511d05a89
Eirik Kvistad <eirik.kvistad@gmail.com>
2021-03-01 15:12:07 +0100

    more comments to get.js

--- src/utils/get.js
+++ src/utils/get.js
@@ -1,8 +1,18 @@
 // denne fila bruker jeg så jeg slipper å skrive firebase-requests i hver eneste vue-fil.
 // Get.js dreier seg spesifikt om å HENTE data. funksjoner for SKRIVE data er definert i create.js
 
+// må importeres i den componenten du ønsker, feks:
+//import {getData} from '@/utils/get.js'
+
+// når du skal bruke den, skriver du:
+
+//    getData(3, "praksis").then(res => {
+//          console.log(res)
+//    })
+
 import { db } from '@/firebase.js'
 
+// bare en collection
 const getData = async (limit, collection) => {
   let ref;
 
@@ -24,6 +34,7 @@ const getData = async (limit, collection) => {
   return res
 }
 
+// flere collections. @param collections forventes som array, f.eks ["users", "prosjekter"]
 const multipleCols = async (limit, collections) => {
     return Promise.all(collections.map(col => {
         const res =  getData(limit, col)
