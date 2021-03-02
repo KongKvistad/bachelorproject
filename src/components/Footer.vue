78568908c95501ff3369488cbbaf103e94f933f8
Mari Rogne <marirosta@hotmail.com>
2021-03-02 14:11:49 +0100

    Footer responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,19 +5,19 @@
 
             </div>
             <div class="row footer-bottom align-items-end">
-                <div class="col-6">
+                <div class="col-md-6">
                     <a href="/">
                         <img src="../assets/images/ntnu_logo.png" alt="">
                     </a>
                 </div>
-                <div class="col-3">
+                <div class="col-md-3">
                     <h6>Trenger du hjelp?</h6>
                     <ul>
                         <li><a href="/">Om BeNet</a></li>
                         <li><a href="#">Kontakt</a></li>
                     </ul>
                 </div>
-                <div class="col-3">
+                <div class="col-md-3">
                     <h6>Andre tjenester</h6>
                     <ul>
                         <li><a href="https://www.ntnu.no">www.ntnu.no</a></li>
