66d546a70eac1cb8b317e4b3e8295c979c399ecf
Eirik Kvistad <eirik.kvistad@gmail.com>
2021-05-05 14:48:33 +0200

    name instead of id

--- src/components/Match.vue
+++ src/components/Match.vue
@@ -2,10 +2,10 @@
 <b-list-group-item style="width: 100%;">
     <b-row>
         <b-col cols="8">
-            <h3 class="h5">{{item.comp}}</h3>
+            <h3 class="h5">{{item.compName}}</h3>
         </b-col>
         <b-col cols="4">
-            <h3 class="h5">{{item.stud}}</h3>
+            <h3 class="h5">{{item.studName}}</h3>
         </b-col>
         
         </b-row>
