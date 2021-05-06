c6627e7744d4a29d4436b89bf6309d6235830b87
Sarah Berg <sarahvb@stud.ntnu.no>
2021-05-06 12:33:22 +0200

    css

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -39,6 +39,8 @@
                         <h1>{{activeChoice}}</h1>
                     </div>
                 </div>
+                 <p v-if="activeChoice == 'historikk'">Historisk oversikt over alle samarbeid i BeNet.</p>
+                 
                 <div v-if="activeChoice == 'overordnet'" class="newCoop">
                     <p>Nye avtaler</p>
                     <p><span>X</span> Samarbeid mangler godkjenning</p>
@@ -49,6 +51,9 @@
                     </b-container>
                     <b-container v-else-if="activeChoice =='brukere' && studentData && companyData">
                         <h2 class="mt-5 mb-4">Studenter</h2>
+                        <p>Dette er alle studentene som deltar i årets gjennomføring av praksisemnet. <br>Se status 
+                            status for hver enkelt student i både praksis og prosjekt.
+                        </p>
                         <MatrixMenu2
                         
                         :data="studentData"
@@ -60,6 +65,8 @@
                             </template>
                         </MatrixMenu2>
                         <h2 class="mt-5 mb-4">Bedrifter</h2>
+                        <p>Dette er alle bedriftene registrert i BeNet.
+                        </p>
                         <grid :cols="companyData.cols" :sort="true" :rows="companyData.rows" />
                     </b-container>
 
